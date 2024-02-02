18f0ecc980cf21abbdcf4d1e917792ca08d18d23
oneul0 <raeque@naver.com>
2024-02-02 15:41:16 +0900

    1920-second_commit

--- back/구현/1920.cpp
+++ back/구현/1920.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
+#include <set>
 
 using namespace std;
 
+int m, a , find_e, ans;
+//vector<int> v;
+set<int> v;
+
 int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> m;
     
+    for(int i = 0; i<m; i++){
+        cin >> a;
+        v.insert(a);
+    }
+    cin >> m;
+    for(int i = 0; i<m; i++){
+        cin >> find_e;
+        if(v.find(find_e) != v.end()) ans = 1;
+        else ans =0;
+        cout << ans <<'\n';
+    }
     return 0;
 }
