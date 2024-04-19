9d5193bad720db937dba4d625fb612464f1bbbc0
oneul0 <raeque@naver.com>
2024-04-19 17:51:21 +0900

    n+1 card game

--- back/수학/정수론/2247.cpp
+++ back/수학/정수론/2247.cpp
@@ -6,9 +6,9 @@ const int mod = 1000000;
 long long CSOD(int n)
 {
     long long sum=0;
-    for(int i = 2; i*i<=n; i++)
+    for(long long i = 2; i*i<=n; i++)
     {
-        int j = n/i;
+        long long j = n/i;
         sum += i * (j - i + 1) + (j - i) * (j + i + 1) / 2;
     }
     return sum%mod;
