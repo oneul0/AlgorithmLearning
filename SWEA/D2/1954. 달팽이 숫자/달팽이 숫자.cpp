c33a7582d63411e963f7692032a970d3e531bfee
oneul0 <54806810+oneul0@users.noreply.github.com>
2024-04-22 15:34:21 +0900

    [D2] Title: 달팽이 숫자, Time: 9 ms, Memory: 13,388 KB -BaekjoonHub

---
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include <vector>
+
+using namespace std;
+int T,n;
+//y+1, x-1, y-1, x+1
+int direct[4][2] = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+vector<vector<int>> solution(int n){
+	vector<vector<int>> ans(n, vector<int>(n,0));
+	int cnt =1, flag = n*n-1;
+	ans[0][0] = 1;
+	int cx = 0, cy=0;
+	int dir = 0;
+	while(flag--){
+		cnt++;
+		//다음 블록 체크
+		int nx = cx+direct[dir][0];
+		int ny = cy+direct[dir][1];
+		//방향을 바꿔야 하는가?
+		if(nx >= n || ny >= n || nx < 0 || ny < 0 || ans[nx][ny] != 0){
+			if(dir >= 3) dir = 0;
+			else dir++;
+			nx = cx+direct[dir][0];
+			ny = cy+direct[dir][1];
+			cx = nx;
+			cy = ny;
+		}
+		else{
+			cx = nx;
+			cy = ny;
+		}
+		ans[cx][cy] = cnt;
+	}
+	 
+	return ans;
+}
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	
+	cin>>T;
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+		cin >> n;
+		cout <<"#"<<test_case<<'\n';
+		vector<vector<int>> v = solution(n);
+		for(int i=0; i<n; i++){
+			for(int j = 0; j<n; j++){
+				cout << v[i][j] << " ";
+			}
+			cout << '\n';
+		}
+	}
+	return 0;
+}
