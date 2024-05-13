399474252be480040088b92ef92a731c9ab59703
oneul0 <54806810+oneul0@users.noreply.github.com>
2024-05-13 21:52:46 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 212 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -15,7 +15,7 @@ public class Main {
             a = sc.nextInt();
             b = sc.nextInt();
             c = sc.nextInt();
-            if(a == 0 && b == 0 && c == 0)
+            if(a +b+ c == 0)
                 break;
             if(a == b && b == c)
                 System.out.println("Equilateral");
