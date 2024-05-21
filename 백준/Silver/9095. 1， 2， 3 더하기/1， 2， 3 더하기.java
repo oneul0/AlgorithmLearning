d91ad6fb90b5b6b3d2864cae1166aa79ebf67711
oneul0 <54806810+oneul0@users.noreply.github.com>
2024-05-21 21:07:44 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 140 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -8,13 +8,16 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[11];
+        dp[1] = 1; dp[2] = 2; dp[3] = 4;
         for(int i = 1; i <= n; i++) {
             int num = Integer.parseInt(br.readLine());
-            int[] dp = new int[11];
-            dp[1] = 1; dp[2] = 2; dp[3] = 4;
-            for(int j = 4; j<=num; j++){
-                dp[j] = dp[j-1] + dp[j-2] + dp[j-3];
+            if(dp[num] == 0) {
+                for(int j = 4; j<=num; j++){
+                    dp[j] = dp[j-1] + dp[j-2] + dp[j-3];
+                }    
             }
+            
             bw.write(dp[num] + "\n");
         }
         br.close();
