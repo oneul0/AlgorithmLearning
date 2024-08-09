ecf8547c3fd3278d4182c6e881d46df8fecdc5a9
oneul0 <54806810+oneul0@users.noreply.github.com>
2024-08-09 10:07:42 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.29 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -7,16 +7,27 @@ class Solution {
         for(int i = 0; i<n+1; i++){
             tree.add(new ArrayList<>());
         }
+        // 1. 무방향 그래프로 바꾸기
         for(int[] wire : wires){
             tree.get(wire[0]).add(wire[1]);
             tree.get(wire[1]).add(wire[0]);
         }
         
+        // 2. 탐색을 시작할 정점과 끊어낼 정점 정하기(어느 간선을 끊을지)
         for(int[] wire : wires){
-            int u = wire[0], v=wire[1];
             boolean[] visited = new boolean[n+1];
-            visited[v] = true;
-            int count = dfs(u,visited);
+            //주어진 간선 정보는 이어질 수 있는 간선의 모든 경우의 수이므로
+            //wires를 순회하는 것 만으로도 모든 경우의 수를 구할 수 있음
+            //이어진 간선을 끊기 위해 v2를 구해 방문처리
+            int v1 = wire[0], v2=wire[1];
+            // 2.1 간선 끊기
+            visited[v2] = true;
+            
+            //dfs가 더해준 count값 받기
+            // 2.2 트리 하나의 정점 수 구하기
+            int count = dfs(v1,visited);
+            //짝수로 만들어서 나뉜 전력망 중 어느것으로 빼도 같은 값이 나오도록
+            // 2.3 차이가 최소가 될 때의 개수 구하기
             answer = Math.min(answer, Math.abs(n-2*count));
         }
         
