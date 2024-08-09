7250614955a7a58383d20059b1e7bc9eff3b85f2
oneul0 <54806810+oneul0@users.noreply.github.com>
2024-08-09 13:25:12 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.15 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -2,48 +2,49 @@ import java.util.*;
 class Solution {
     int[] dx = {-1,1,0,0};
     int[] dy = {0,0,-1,1};
+    int n,m;
     public int[] solution(String[][] places) {
         int[] answer = new int[places.length];
+        this.n = places.length;
+        this.m = places[0].length;
         for(int i = 0; i<places.length; i++){
-            answer[i] = init(places[i]);
+            answer[i] = isValid(places[i]);
         }
+        
         return answer;
     }
     
-    int init(String[] place){
-        //P 찾기
-        for(int i = 0; i<place.length; i++){
-            for(int j = 0; j<place[i].length(); j++){
-                if(place[i].charAt(j) == 'P'){
-                    if(!bfs(i,j,place)) return 0;
-                }
+    
+    int isValid(String[] room){
+    //P찾기    
+        for(int i= 0; i<room.length; i++){
+            for(int j = 0; j<room[0].length(); j++){
+                if(room[i].charAt(j) == 'P')
+                    if(!bfs(i,j,room)) return 0;
             }
         }
         return 1;
+        
     }
     
-    boolean bfs(int sx, int sy,String[] place){
-        boolean[][] room = new boolean[place.length][place[0].length()];
-        Queue<int[]> q = new ArrayDeque<>();
-        q.add(new int[]{sx,sy,0});
-        room[sx][sy] = true;
+    //유효성검증
+    boolean bfs(int sx, int sy, String[] room){
+        Queue<int[]> q= new ArrayDeque<>();
+        boolean[][] chk = new boolean[n][m];
+        q.offer(new int[]{sx,sy,0});
+        chk[sx][sy] = true;
         while(!q.isEmpty()){
             int[] cur = q.remove();
             
-            for(int d= 0;d<4;d++){
-                int nx = cur[0]+dx[d];
-                int ny = cur[1]+dy[d];
-                if(cur[2]+1 <= 2 && nx>=0 && nx<place.length && ny>=0 && ny < place[0].length() && !room[nx][ny] && place[nx].charAt(ny) != 'X'){
-                    if(place[nx].charAt(ny) == 'P') return false;
-                    
-                    q.add(new int[]{nx,ny,cur[2]+1});
-                    room[nx][ny] = true;     
-                }
-                
-
+            for(int i =0; i<4; i++){
+                int nx = cur[0]+dx[i];
+                int ny = cur[1]+dy[i];
+                if(cur[2] >= 2 || nx<0||ny<0||nx>=n||ny>=m||chk[nx][ny]||room[nx].charAt(ny) == 'X') continue;
+                if(room[nx].charAt(ny) == 'P') return false;
+                q.offer(new int[]{nx,ny,cur[2]+1});
+                chk[nx][ny] = true;
             }
         }
-        
         return true;
     }
 }
