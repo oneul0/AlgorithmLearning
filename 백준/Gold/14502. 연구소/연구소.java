7b6766926a00a68487e1047024b748e7fe1a9325
oneul0 <54806810+oneul0@users.noreply.github.com>
2024-08-14 21:41:09 +0900

    [Gold IV] Title: 연구소, Time: 360 ms, Memory: 85508 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,106 +1,103 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static final int[] dx = {-1, 1, 0, 0};
-    static final int[] dy = {0, 0, -1, 1};
-    static int[][] lab;
-    static int N, M;
 
-    static ArrayList<Pair> virus = new ArrayList<>();
-    static ArrayList<Pair> walls = new ArrayList<>();
-    static int maxSafeTileCount = 0;
+    static int[][] map;
+    static List<Pair> virus = new ArrayList<>();
+    static int maxSafeBlockCnt = 0;
+    static final int[] dx = {-1,1,0,0};
+    static final int[] dy = {0,0,-1,1};
+    static int N,M;
+    static int answer = 0;
     static ArrayList<Integer> results = new ArrayList<>();
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
+        //input
         String[] input = br.readLine().split(" ");
         N = Integer.parseInt(input[0]);
         M = Integer.parseInt(input[1]);
-        lab = new int[N][M];
-
-        // 입력
-        for (int i = 0; i < N; i++) {
-            input = br.readLine().split(" ");
-            for (int j = 0; j < M; j++) {
-                lab[i][j] = Integer.parseInt(input[j]);
-                if (lab[i][j] == 0) maxSafeTileCount++;
-                else if (lab[i][j] == 2) virus.add(new Pair(i, j));
+        map = new int[N][M];
+        for(int i = 0; i<N; i++){
+            String[] line = br.readLine().split(" ");
+            for(int j = 0; j<M; j++){
+                map[i][j] = Integer.parseInt(line[j]);
+                if(map[i][j] == 0) maxSafeBlockCnt++;
+                else if(map[i][j] == 2) virus.add(new Pair(i, j));
             }
         }
-
-        // 벽을 세우는 조합 생성
-        combine(0,0, lab);
-
-        // 안전 영역의 최대
-        int answer = results.stream().mapToInt(Integer::intValue).max().orElse(0);
-        bw.write(String.valueOf(answer));
+        dfs(0,0,map);
+        answer = results.stream().mapToInt(Integer::intValue).max().orElse(0);
+        bw.write(Integer.toString(answer));
         bw.flush();
+        br.close();
+        bw.close();
     }
 
-    // 벽을 세우는 조합 생성
-    static void combine(int depth, int start, int[][] lab) {
-        if (depth == 3) {
-            // 벽을 세운 후 바이러스 퍼뜨리기
-            results.add(bfs(lab));
+
+    //dfs
+    //벽 조합 세우고
+    static void dfs(int depth, int start, int[][] map){
+        if(depth == 3){
+            results.add(bfs(map));
             return;
         }
 
-        for (int i = start; i < N * M; i++) {
-            int x = i / M;
-            int y = i % M;
-            if (lab[x][y] == 0) {
-                lab[x][y] = 1; // 벽 세우기
-                combine(depth + 1,i + 1, lab);
-                lab[x][y] = 0; // 상태 복원
+        for(int i = start; i<N*M; i++){
+            int x = i/M;
+            int y = i%M;
+            if(map[x][y] == 0){
+                map[x][y] = 1;
+                dfs(depth+1,i+1, map);
+                map[x][y] = 0;
             }
         }
+
     }
 
-    // 바이러스를 퍼뜨리기
-    static int bfs(int[][] lab) {
-        int cnt = 0;
+    //bfs
+    //팀색
+    static int bfs(int[][] map){
         Queue<Pair> q = new ArrayDeque<>();
         boolean[][] visited = new boolean[N][M];
-
-        // 초기 바이러스 위치 큐에 추가
-        for (Pair v : virus) {
-            q.add(new Pair(v.getX(), v.getY()));
-            visited[v.getX()][v.getY()] = true;
+        int cnt = 0;
+        for(Pair p : virus){
+            q.offer(new Pair(p.X(), p.Y()));
         }
 
-        while (!q.isEmpty()) {
-            Pair cur = q.poll();
-            int x = cur.getX();
-            int y = cur.getY();
+        while(!q.isEmpty()){
+            Pair cur = q.remove();
 
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if (nx >= 0 && ny >= 0 && nx < N && ny < M && !visited[nx][ny] && lab[nx][ny] == 0) {
-                    visited[nx][ny] = true;
+            for(int i = 0; i<4; i++){
+                int nx = cur.X() + dx[i];
+                int ny = cur.Y() + dy[i];
+                if(nx>=0&&ny>=0&&nx<N&&ny<M&&!visited[nx][ny]&&map[nx][ny]==0){
                     q.offer(new Pair(nx, ny));
-                    cnt++; // 바이러스 count
+                    visited[nx][ny] = true;
+                    cnt++;
                 }
             }
         }
-        cnt += 3;
-        return maxSafeTileCount-cnt; // 퍼뜨린 바이러스 수 반환
+
+        return  maxSafeBlockCnt-cnt-3;
     }
+
+
 }
 
-class Pair {
+
+class Pair{
     int x, y;
-    public Pair(int x, int y) {
+    Pair(int x, int y){
         this.x = x;
         this.y = y;
     }
-    public int getX() {
+    int X(){
         return x;
     }
-    public int getY() {
+    int Y(){
         return y;
     }
 }
