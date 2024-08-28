7df7e435503824c8555542b09d9434cff561fd04
oneul0 <54806810+oneul0@users.noreply.github.com>
2024-08-28 20:15:10 +0900

    [level 3] Title: 합승 택시 요금, Time: 39.08 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,70 +1,65 @@
 import java.util.*;
-
 class Solution {
-    ArrayList<ArrayList<Pair>> gh = new ArrayList<>();
-    int n;
-
+    int n, s, a, b, max = Integer.MAX_VALUE;
+    ArrayList<ArrayList<Pair>> gr = new ArrayList<>();
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        this.n = n;
-
-        for (int i = 0; i <= n; i++) {
-            gh.add(new ArrayList<>());
+        this.n = n; this.s = s; this.a = a; this.b = b;
+        for(int i = 0; i<= n; i++){
+            gr.add(new ArrayList<>());
         }
-
-        for (int[] fare : fares) {
-            gh.get(fare[0]).add(new Pair(fare[1], fare[2]));
-            gh.get(fare[1]).add(new Pair(fare[0], fare[2]));
+        
+        int answer = max;
+        
+        for(int[] fare : fares){
+            gr.get(fare[0]).add(new Pair(fare[1], fare[2]));
+            gr.get(fare[1]).add(new Pair(fare[0], fare[2]));
         }
-
-        // s,a,b에서 모든 노드까지의 최단 거리 계산
-        int[] distFromS = dijkstra(s);
-        int[] distFromA = dijkstra(a);
-        int[] distFromB = dijkstra(b);
-
-        int minDistance = Integer.MAX_VALUE;
-
-        for (int i = 1; i <= n; i++) {
-            minDistance = Math.min(minDistance, distFromA[i] + distFromB[i] + distFromS[i]);
+        
+        int[] toS = dijkstra(s);
+        int[] toA = dijkstra(a);
+        int[] toB = dijkstra(b);
+        
+        
+        for(int i = 1; i<=n; i++){
+            answer = Math.min(answer, toS[i]+toA[i]+toB[i]);
         }
-
-        return minDistance;
+        
+        return answer;
     }
-
-    public int[] dijkstra(int start) {
+    
+    int[] dijkstra(int start){
+        int[] dist = new int[n+1];
+        Arrays.fill(dist, max);
         PriorityQueue<Pair> pq = new PriorityQueue<>();
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-
         pq.offer(new Pair(start, 0));
         dist[start] = 0;
-
-        while (!pq.isEmpty()) {
-            Pair cur = pq.poll();
-            if (cur.cost > dist[cur.node]) continue;
-
-            for (Pair nxt : gh.get(cur.node)) {
-                int nxtCost = nxt.cost + cur.cost;
-                if (nxtCost < dist[nxt.node]) {
-                    dist[nxt.node] = nxtCost;
-                    pq.offer(new Pair(nxt.node, nxtCost));
-                }
+        
+        while(!pq.isEmpty()){
+            Pair cur = pq.remove();
+            if(cur.cost > dist[cur.node]) continue;
+            for(Pair nxt : gr.get(cur.node)){
+                int nxtCost = cur.cost+nxt.cost;
+                if(dist[nxt.node] < nxtCost) continue;
+                pq.offer(new Pair(nxt.node, nxtCost));
+                dist[nxt.node] = nxtCost;
             }
         }
-
         return dist;
     }
 }
 
-class Pair implements Comparable<Pair> {
+class Pair implements Comparable<Pair>{
     int node, cost;
-
-    public Pair(int node, int cost) {
+    public Pair(int node, int cost){
         this.node = node;
         this.cost = cost;
     }
-
+    
     @Override
-    public int compareTo(Pair o) {
-        return Integer.compare(this.cost, o.cost);
+    public int compareTo(Pair p){
+        return Integer.compare(this.cost, p.cost);
     }
 }
+
+//시작 지점으로부터 각 지점까지의 거리만 구하는 다익스트라
+
