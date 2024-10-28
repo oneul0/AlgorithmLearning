4881c211f3cfa1b624825c306ebfe2861bbfd74c
oneul0 <54806810+oneul0@users.noreply.github.com>
2024-10-28 21:46:37 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 90.93 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,63 +1,67 @@
 import java.util.*;
 class Solution {
-    int[] discount = {10,20,30,40}, answer = new int[2];
-    ArrayList<Pair> emoPrice = new ArrayList<>();
+    int[] discount = {10, 20, 30, 40}, answer = new int[2], emoticons;
     int[][] users;
-    int[] emoticons;
+    ArrayList<Pair> result = new ArrayList<>();
     public int[] solution(int[][] users, int[] emoticons) {
-        this.users = users;
-        this.emoticons = emoticons;
-        for(int i = 0; i<7; i++) emoPrice.add(new Pair(0,0));
-        dfs(0, emoticons.length, users, emoticons);
+        this.users = users; this.emoticons = emoticons;
+        dfs(0, new int[emoticons.length]);
+        result.sort(Comparator.reverseOrder());
+        answer[0] = result.get(0).subs;
+        answer[1] = result.get(0).total;
         return answer;
     }
     
-    void dfs(int emoticon, int emoCount, int[][] users, int[] emoticons){
-        if(emoticon < emoCount){
-            for(int i = 0; i<discount.length; i++){
-                emoPrice.get(emoticon).saledPrice = emoticons[emoticon] - ((emoticons[emoticon]/100)*discount[i]);
-                emoPrice.get(emoticon).discount = discount[i];
-                dfs(emoticon+1, emoCount, users, emoticons);
-            }
-        }        
-        else{
-            int subs = 0, totalCart = 0;
-            
+    //모든 경우 다 구해보기
+    void dfs(int depth, int[] cur_discount){
+        //기저조건
+        if(depth >= emoticons.length){
+            int new_subs = 0, total_cart = 0;
+            //유저별로 
             for(int i = 0; i<users.length; i++){
-                int cart = 0;
-                for(int j = 0; j<emoCount; j++){
-                    if(emoPrice.get(j).discount >= users[i][0]){
-                        cart += emoPrice.get(j).saledPrice;
+                int user_cart = 0;
+                //이모티콘 샀는지 비교
+                for(int j = 0; j<emoticons.length; j++){
+                    int saled = calcSale(emoticons[j], cur_discount[j]);
+                    //유저가 못 참고 사는지 확인
+                    if(cur_discount[j] >= users[i][0]){
+                        user_cart += saled;
                     }
                 }
-                //구독 or 구매
-                if(cart >= users[i][1]){
-                    subs++;
-                }
-                else{
-                    totalCart += cart;
-                }
+                
+                if(user_cart >= users[i][1]) new_subs++;
+                else total_cart += user_cart;
             }
-            
-            if(subs > answer[0]){
-                answer[0] = subs;
-                answer[1] = totalCart;
-            }
-            else if(subs == answer[0] && totalCart > answer[1]){
-                answer[1] = totalCart;
-            }
+            result.add(new Pair(new_subs, total_cart));
             return;
         }
+        
+        for(int i = 0 ; i<4; i++){
+            cur_discount[depth] = discount[i];
+            dfs(depth+1, cur_discount);
+        }
+        
+    }
+    
+    //세일된 가격
+    int calcSale(int origin_price, int sale_pctg){
+        return (origin_price * (100 - sale_pctg)) / 100;
     }
 }
 
-class Pair{
-    int saledPrice, discount;
-    Pair(int saledPrice, int discount){
-        this.saledPrice = saledPrice;
-        this.discount = discount;
+class Pair implements Comparable<Pair>{
+    int subs, total;
+    
+    Pair(int subs, int total){
+        this.subs = subs;
+        this.total = total;
+    }
+    
+    @Override
+    public int compareTo(Pair o) {
+        if (subs == o.subs) {
+            return total - o.total;
+        }
+        return subs - o.subs;
     }
 }
-
-
-
