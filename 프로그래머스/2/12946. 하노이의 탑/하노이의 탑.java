8ce32e3b52d6eafdf5cfea92a8a3bbc80432438b
oneul0 <54806810+oneul0@users.noreply.github.com>
2024-11-02 14:47:37 +0900

    [level 2] Title: 하노이의 탑, Time: 5.40 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,22 +1,22 @@
 import java.util.*;
 class Solution {
     ArrayList<int[]> tmp = new ArrayList<>();
+    int n;
     public int[][] solution(int n) {
-        dfs(n, 1, 2, 3);
-        int[][] answer = new int[tmp.size()][];
+        this.n = n;
+        dfs(1, 2, 3, 0);
+        int[][] answer = new int[tmp.size()][2];
         for(int i = 0; i<tmp.size(); i++){
             answer[i] = tmp.get(i);
         }
         return answer;
     }
     
-    void dfs(int n, int from, int by, int to){
-        //기저사례
-        if(n == 0) return;
+    void dfs(int start, int by, int end, int depth){
+        if(depth == n) return;
         
-        dfs(n-1, from, to, by);
-        tmp.add(new int[]{from, to});
-        dfs(n-1, by, from, to);
-        
+        dfs(start, end, by, depth+1);
+        tmp.add(new int[]{start, end});
+        dfs(by, start, end, depth+1);
     }
 }
