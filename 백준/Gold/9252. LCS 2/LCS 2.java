d111c75a3c7248e279c800848847a24ae0d80bbf
oneul0 <54806810+oneul0@users.noreply.github.com>
2024-11-18 16:42:41 +0900

    [Gold IV] Title: LCS 2, Time: 156 ms, Memory: 20180 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -0,0 +1,49 @@
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        StringBuilder sb = new StringBuilder();
+
+        int n = str1.length();
+        int m = str2.length();
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        bw.write(dp[n][m] + "\n");
+
+        int i = n, j = m;
+        while (i > 0 && j > 0) {
+            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                sb.append(str1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] >= dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        if (sb.length() > 0) {
+            sb.reverse();
+            bw.write(sb.toString());
+        }
+
+        br.close();
+        bw.close();
+    }
+}
