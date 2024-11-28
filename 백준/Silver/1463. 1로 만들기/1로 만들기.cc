5ee8e6ffc3618632168c47116a6eeb90a9696607
oneul0 <54806810+oneul0@users.noreply.github.com>
2024-11-28 20:41:59 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    size_t n;
+    cin >> n;
+
+    vector<int> cnt(n+1);
+    cnt[1] = 0;
+    for(int i = 2 ; i<=n; i++){
+        cnt[i] = cnt[i-1]+1;
+        if(i%3 == 0) cnt[i] = min(cnt[i], cnt[i/3]+1);
+        if(i%2 == 0) cnt[i] = min(cnt[i], cnt[i/2]+1);
+    }
+    cout << cnt[n];
+    return 0;
+}
