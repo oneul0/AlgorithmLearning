24ac7c2c9722faa6d6e0df777837487741910e1e
oneul0 <54806810+oneul0@users.noreply.github.com>
2024-12-16 22:36:23 +0900

    [Gold IV] Title: 여행 가자, Time: 160 ms, Memory: 17768 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,70 +1,56 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int N, M;
-    static StringTokenizer st;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int[] parent;
-    public static void main(String[] args) throws IOException {
+    static StringTokenizer st;
+    static int[] pr;
+    static int N, M;
+    public static void main(String[] args) throws Exception{
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-        parent = new int[N+1];
+        pr = new int[N+1];
 
-        for(int i = 0; i<=N; i++){
-            parent[i] = i;
-        }
+        for(int i = 0; i<=N; i++) pr[i] = i;
 
         for(int i = 1; i<=N; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j= 1; j<=N; j++){
-                int tmp = Integer.parseInt(st.nextToken());
-                if(tmp == 1){
+            for(int j = 1; j<=N; j++){
+                if(st.nextToken().equals("1")){
                     union(i, j);
                 }
             }
         }
 
-        boolean flag = true;
+        boolean fg = true;
         st = new StringTokenizer(br.readLine());
-        int sx = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
         for(int i = 1; i<M; i++){
-            int p = Integer.parseInt(st.nextToken());
-            if(find(sx) != find(p)){
-                flag = false;
+            if(find(s) != find(Integer.parseInt(st.nextToken()))) {
+                fg = false;
                 break;
             }
         }
 
-        if(flag){
-            bw.write("YES");
-        }
-        else{
-            bw.write("NO");
-        }
+        if(fg) bw.write("YES");
+        else bw.write("NO");
         bw.flush();
         br.close();
         bw.close();
     }
 
-    static int find(int x){
-        if(x == parent[x]) return x;
-
-        return parent[x] = find(parent[x]);
+    static int find(int a){
+        if(pr[a] == a) return a;
+        return find(pr[a]);
     }
 
-    static void union(int x, int y){
-        x = find(x);
-        y = find(y);
-        if(x!=y){
-            if(x>y){
-                parent[x] = y;
-            }
-            else{
-                parent[y] = x;
-            }
+    static void union(int a, int b){
+        int x = find(a);
+        int y = find(b);
+        if(x != y){
+            if(x > y) pr[x] = y;
+            else pr[y] = x;
         }
-
     }
 }
