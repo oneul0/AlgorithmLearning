dc67bb157eefc84a22d0b3d74e9c3d7055db8ef9
oneul0 <54806810+oneul0@users.noreply.github.com>
2025-01-04 20:07:55 +0900

    [Gold V] Title: A와 B, Time: 116 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static String s, t;
+    public static void main(String[] args) throws Exception{
+        s = br.readLine();
+        t = br.readLine();
+
+        bw.write(canTrans());
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    static String canTrans() {
+        StringBuilder str = new StringBuilder(t);
+
+        while(str.length() > s.length()){
+            if(str.charAt(str.length() -1) == 'A') str.deleteCharAt(str.length()-1);
+            else{
+                str.deleteCharAt(str.length()-1);
+                str.reverse();
+            }
+        }
+
+        return str.toString().equals(s) ? "1" : "0";
+    }
+
+
+}
