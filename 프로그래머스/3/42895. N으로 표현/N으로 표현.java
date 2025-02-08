e827af2b12e3962cf2916d9228df28409f98565b
oneul0 <54806810+oneul0@users.noreply.github.com>
2025-02-08 20:57:35 +0900

    [level 3] Title: N으로 표현, Time: 48.67 ms, Memory: 92.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int N, int number) {
+        if(N == number) return 1;
+        // N을 i번 사용했을 때(dp[i]) 만들 수 있는 숫자들의 집합
+        ArrayList<HashSet<Integer>> dp = new ArrayList<>();
+
+        for (int i = 0; i <= 8; i++) {
+            dp.add(new HashSet<>());
+        }
+
+        dp.get(1).add(N);
+
+        for (int usedN = 2; usedN <= 8; usedN++) {
+            //문자열 덧셈 연산
+            int repeatedN = Integer.parseInt(String.valueOf(N).repeat(usedN));
+            dp.get(usedN).add(repeatedN);
+            if (repeatedN == number) return usedN;
+
+            for (int i = 1; i < usedN; i++) {
+                for (Integer num1 : dp.get(i)) {
+                    for (Integer num2 : dp.get(usedN - i)) {
+                        // 사칙연산
+                        dp.get(usedN).add(num1 + num2);
+                        dp.get(usedN).add(num1 - num2);
+                        dp.get(usedN).add(num1 * num2);
+                        if (num2 != 0) dp.get(usedN).add(num1 / num2);
+
+                        if (num1 + num2 == number || 
+                            num1 - num2 == number || 
+                            num1 * num2 == number || 
+                            (num2 != 0 && num1 / num2 == number)) {
+                            return usedN;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
