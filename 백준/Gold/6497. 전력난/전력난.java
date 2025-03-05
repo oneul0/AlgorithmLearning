89c9c9abf10080a30e28ab242137b1901da735d0
oneul0 <54806810+oneul0@users.noreply.github.com>
2025-03-05 10:41:31 +0900

    [Gold IV] Title: 전력난, Time: 816 ms, Memory: 249436 KB -BaekjoonHub

---
+++ 백준/Gold/6497. 전력난/전력난.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  static int m, n;
+  static int[] parents;
+  static class Node implements Comparable<Node> {
+    int from, to, cost;
+
+    Node(int from, int to, int cost) {
+      this.from = from;
+      this.to = to;
+      this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+      return Integer.compare(this.cost, o.cost);
+    }
+  }
+
+  static void union(int from, int to) {
+    from = find(from);
+    to = find(to);
+
+    if(from != to){
+      parents[to] = from;
+    }
+  }
+
+  static int find(int x) {
+      int root = x;
+
+      while (parents[root] != root) {
+        root = parents[root];
+      }
+
+      for (int tmp; x != root; x = tmp) {
+        tmp = parents[x];
+        parents[x] = root;
+      }
+
+      return root;
+  }
+
+  public static void main(String[] args) throws IOException {
+    StringTokenizer st;
+
+    while(true) {
+      st = new StringTokenizer(br.readLine());
+      m = Integer.parseInt(st.nextToken());
+      n = Integer.parseInt(st.nextToken());
+      if(m == 0 && n == 0) break;
+      PriorityQueue<Node> pq = new PriorityQueue<>();
+      int ans = 0;
+      for(int i = 0; i<n; i++) {
+        st = new StringTokenizer(br.readLine());
+        int from = Integer.parseInt(st.nextToken());
+        int to = Integer.parseInt(st.nextToken());
+        int cost = Integer.parseInt(st.nextToken());
+        pq.add(new Node(from, to, cost));
+        ans += cost;
+      }
+
+      parents = new int[m];
+      for(int i = 0; i < m; i++) {
+        parents[i] = i;
+      }
+
+      int con = 0;
+      while(con < m-1){
+        Node node = pq.remove();
+        if(find(node.from) != find(node.to)){
+          union(node.from, node.to);
+          con++;
+          ans -= node.cost;
+        }
+      }
+
+      bw.write(ans+"\n");
+    }
+    bw.flush();
+    br.close();
+    bw.close();
+  }
+
+}
