0b152488591f2ef85788adcf2c5338bada0f4f8e
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2025-08-13 14:29:32 +0900

    Title: 겹치지 않게 선분 고르기, Time: 266ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250813/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 250813/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Main {
-    static int answer = 0, limit = 0;
+    static int answer = 0;
     static int[][] segments;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -10,23 +10,22 @@ public class Main {
         for (int i = 0; i < n; i++) {
             segments[i][0] = sc.nextInt();
             segments[i][1] = sc.nextInt();
-            limit = Math.max(limit, segments[i][1]);
         }
         Arrays.sort(segments, (a, b) -> Integer.compare(a[0], b[0]));
         //시작 x값, 선분 수
-        findMaxLine(1, 0);
+        findMaxLine(1, 0, 0);
         System.out.print(answer);
     }
 
-    public static void findMaxLine(int depth, int count){
-        if(depth >= limit){
+    public static void findMaxLine(int cx, int count, int idx){
+        if(idx >= segments.length){
             answer = Math.max(answer, count);
             return;
         }
 
-        for(int[] seg : segments){
-            if(seg[0]>=depth){
-                findMaxLine(seg[1]+1, count+1);
+        for(int i = idx; i<segments.length; i++){
+            if(segments[i][0]>cx){
+                findMaxLine(segments[i][1], count+1, i+1);
             }
         }
     }
