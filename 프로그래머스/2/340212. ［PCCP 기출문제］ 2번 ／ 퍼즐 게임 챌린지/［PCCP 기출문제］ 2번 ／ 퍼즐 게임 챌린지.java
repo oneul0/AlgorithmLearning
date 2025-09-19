bc7f9dcb9cae3044f1294f63136d384a3b93d87a
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2025-09-19 16:16:50 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 59.20 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,34 +1,48 @@
 class Solution {
     int[] diffs, times;
+    long limit;
+    int size;
     public int solution(int[] diffs, int[] times, long limit) {
         this.diffs = diffs;
         this.times = times;
-        int left = 1;
-        int right = 100000;
+        this.limit = limit;
+        size = diffs.length;
         
-        if(canSolve(left, limit)) return 1;
-        
-        while (left+1 < right) {
-            int mid = (left + right) / 2;
-
-            if (canSolve(mid, limit)) {
-                right = mid;
-            } else {
-                left = mid;
+        int answer = (int) findLevel();
+        return answer;
+    }
+    
+    public long findLevel(){
+        long begin = 1;
+        long end = Integer.MAX_VALUE;
+        long result = end;
+        while(begin <= end){
+            long mid = begin + (end - begin)/2;
+            if(canSolve(mid)){
+                result = mid;
+                end = mid-1;
+            }
+            else{
+                begin = mid+1;
             }
         }
-
-        return right;
+        return result;
     }
-
-    private boolean canSolve(int level, long limit) {
-        long totalTime = times[0];
-        for (int i = 1; i < diffs.length; i++) {
-            if(diffs[i] > level) totalTime += ((times[i] + times[i - 1]) * (diffs[i] - level)) + times[i];
-            else totalTime += times[i];
-            
-            if (totalTime > limit) return false;
+    
+    public boolean canSolve(long lv){
+        long solveTime = 0;
+        for(int i = 0; i<size; i++){
+            if(diffs[i] <= lv){
+                solveTime += times[i];
+            }
+            else{
+                long gap = diffs[i] - (int) lv;
+                solveTime += (times[i] + times[i-1])*gap;
+                solveTime += times[i];
+            }
         }
-        return true;
+        return solveTime <= limit;
     }
 }
+
+//조건을 만족하는 최소의 수(lower bound) 찾기
