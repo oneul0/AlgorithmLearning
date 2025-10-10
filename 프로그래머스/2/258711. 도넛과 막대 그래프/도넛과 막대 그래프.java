8e45a32036667d49abe5a944e4e4a91caeee0c2e
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2025-10-11 00:10:32 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 351.32 ms, Memory: 295 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -8,12 +8,8 @@ class Solution {
             int from = edge[0];
             int to = edge[1];
             
-            if(!nodes.containsKey(from)){
-                nodes.put(from, new int[]{0,0});
-            }
-            if(!nodes.containsKey(to)){
-                nodes.put(to, new int[]{0,0});
-            }
+            nodes.putIfAbsent(from, new int[]{0,0});
+            nodes.putIfAbsent(to, new int[]{0,0});
             
             nodes.get(from)[0]++;
             nodes.get(to)[1]++;
@@ -22,8 +18,7 @@ class Solution {
         for(int key : nodes.keySet()){
             int[] cnt = nodes.get(key);
             
-            //out 2이상, in 0 -> 생성한 정점
-            
+            //out 2이상, in 0 -> 생성한 정점\
             if(cnt[0] >= 2 && cnt[1] == 0){
                 answer[0] = key;
             }
@@ -39,7 +34,10 @@ class Solution {
             }
             
         }
-        
+        //총 그래프의 개수 = root노드의 진출차수의 수
+        //8자 그래프에서 indegree == 2, outdegree == 2 인 노드는 무조건 1개
+        //막대 그래프에서 indegree == 1, outdegree == 0 인 노드는 무조건 1개
+        //도넛 그래프의 수 = root 노드의 진출 차수 - 막대 그래프의 수 - 8자 그래프의 수
         answer[1] = nodes.get(answer[0])[0] - answer[2] - answer[3];
         return answer;
     }
