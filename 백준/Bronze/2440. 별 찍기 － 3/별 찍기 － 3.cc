ab98d379aa739e59bcaaa5fc14de6f5d6687c2c0
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2025-11-24 19:28:03 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    while(n-- > 0){
+        for(int i = 0; i<=n; i++) cout << '*';
+        cout << endl;
+    }
+    return 0;
+}
