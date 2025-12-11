09c5740912772f94438764c5dc21a7e52f367984
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2025-12-11 16:47:13 +0900

    [Gold III] Title: 가희와 탑, Time: 200 ms, Memory: 22376 KB -BaekjoonHub

--- 백준/Gold/24337. 가희와 탑/가희와 탑.java
+++ 백준/Gold/24337. 가희와 탑/가희와 탑.java
@@ -10,34 +10,41 @@ public class Main {
 		int a = Integer.parseInt(st.nextToken());
 		int b = Integer.parseInt(st.nextToken());
 
-		List<Integer> H = new ArrayList<>();
-
 		if (a + b > N + 1) {
-			System.out.print(-1);
+			System.out.println(-1);
 			return;
 		}
 
+		List<Integer> H = new ArrayList<>(N);
+
+		// 왼쪽 증가 부분 (1 ~ a-1)
 		for (int i = 1; i < a; i++) {
 			H.add(i);
 		}
+
+		// peak
 		H.add(Math.max(a, b));
 
+		// 오른쪽 감소 부분 (b-1 ~ 1)
 		for (int i = b - 1; i >= 1; i--) {
 			H.add(i);
 		}
 
-		//padding
-		if (a == 1) {
-			while (H.size() < N) {
-				H.add(1, 1);
-			}
-		} else {
-			while (H.size() < N) {
-				H.add(0, 1);
+		// padding
+		int needed = N - H.size();
+
+		if (needed > 0) {
+			List<Integer> padding = new ArrayList<>(Collections.nCopies(needed, 1));
+
+			if (a == 1) {
+				H.addAll(1, padding);
+			} else {
+				H.addAll(0, padding);
 			}
 		}
-		for (int i : H) {
-			System.out.print(i + " ");
-		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int x : H) sb.append(x).append(" ");
+		System.out.println(sb);
 	}
 }
