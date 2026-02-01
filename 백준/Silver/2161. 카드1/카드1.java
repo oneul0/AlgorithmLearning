cb80eab9f03133d013ffae4f6cb2c2cf18f37ada
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-02-01 11:54:46 +0900

    [Silver V] Title: 카드1, Time: 140 ms, Memory: 14896 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.java
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -8,15 +8,17 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		br.close();
 		Queue<Integer> q = new ArrayDeque<>();
-		for(int i = 1; i<=n; i++){
+		for (int i = 1; i<=n; i++){
 			q.offer(i);
 		}
-		while(!q.isEmpty()){
-			System.out.print(q.poll() +" ");
-			if(q.isEmpty()) break;
-			q.offer(q.poll());
+		//카드가 한 장 남을 때 까지 반복
+		while(q.size() > 1){
+			//제일 위에 있는 카드를 바닥에 버린다
+			System.out.print(q.poll()+" ");
+			//제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다
+			if(!q.isEmpty()) q.offer(q.poll());
 		}
+		System.out.println(q.poll());
 	}
 }
