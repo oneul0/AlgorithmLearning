93dcedd53f333b8a712e4c946a3378bf684a37b2
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-07-11 18:14:04 +0900

    Time: 110 ms (67.77%), Space: 47 MB (90.03%) - LeetHub

---
+++ 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.java
@@ -0,0 +1,61 @@
+class Solution {
+    int answer;
+    int[] values;
+    int[] visitCount;
+    List<int[]>[] gr;
+    int maxTime;
+
+    public int maximalPathQuality(int[] values, int[][] edges, int maxTime) {
+        this.values = values;
+        this.maxTime = maxTime;
+
+        int n = values.length;
+        gr = new ArrayList[n];
+        visitCount = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            gr[i] = new ArrayList<>();
+        }
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int time = edge[2];
+
+            gr[u].add(new int[]{v, time});
+            gr[v].add(new int[]{u, time});
+        }
+
+        visitCount[0] = 1;
+        dfs(0, 0, values[0]);
+
+        return answer;
+    }
+
+    private void dfs(int node, int time, int quality) {
+        if (node == 0) {
+            answer = Math.max(answer, quality);
+        }
+
+        for (int[] edge : gr[node]) {
+            int next = edge[0];
+            int cost = edge[1];
+
+            if (time + cost > maxTime) {
+                continue;
+            }
+
+            boolean firstVisit = visitCount[next] == 0;
+
+            visitCount[next]++;
+
+            dfs(
+                next,
+                time + cost,
+                quality + (firstVisit ? values[next] : 0)
+            );
+
+            visitCount[next]--;
+        }
+    }
+}
