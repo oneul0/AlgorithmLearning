5b1f1b146ee7f20fdbe59c8c4da9468e1775d26f
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-07-22 09:33:07 +0900

    [level 1] Title: 폰켓몬, Time: 4.21 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,16 +1,9 @@
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        int n2 = nums.length/2;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i<nums.length; i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i]) + 1);
-            }
-            else{
-                map.put(nums[i], 1);
-            }
-        }
-        return map.size()>n2 ? n2 : map.size();
+        int n = nums.length;
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) set.add(num);
+        return Math.min(n>>>1, set.size());
     }
 }
