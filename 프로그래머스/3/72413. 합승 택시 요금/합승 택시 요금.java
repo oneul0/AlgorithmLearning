c48f8778c45799435aec67427193bcf0df3e50eb
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-07-30 22:10:11 +0900

    [level 3] Title: 합승 택시 요금, Time: 27.91 ms, Memory: 68.6 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,65 +1,66 @@
 import java.util.*;
 class Solution {
-    int n, s, a, b, max = Integer.MAX_VALUE;
-    ArrayList<ArrayList<Pair>> gr = new ArrayList<>();
+    class Edge implements Comparable<Edge> {
+        int to, cost;
+        Edge(int to, int cost){
+            this.to = to;
+            this.cost = cost;
+        }
+        @Override
+        public int compareTo(Edge o){
+            return Integer.compare(this.cost, o.cost);
+        }
+    }
+    int n;
+    List<List<Edge>> gr = new ArrayList<>();
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        this.n = n; this.s = s; this.a = a; this.b = b;
-        for(int i = 0; i<= n; i++){
+        this.n = n;
+        for(int i = 0; i<=n; i++){
             gr.add(new ArrayList<>());
         }
-        
-        int answer = max;
-        
         for(int[] fare : fares){
-            gr.get(fare[0]).add(new Pair(fare[1], fare[2]));
-            gr.get(fare[1]).add(new Pair(fare[0], fare[2]));
+            gr.get(fare[0]).add(new Edge(fare[1], fare[2]));
+            gr.get(fare[1]).add(new Edge(fare[0], fare[2]));    
         }
         
-        int[] toS = dijkstra(s);
-        int[] toA = dijkstra(a);
-        int[] toB = dijkstra(b);
-        
-        
-        for(int i = 1; i<=n; i++){
-            answer = Math.min(answer, toS[i]+toA[i]+toB[i]);
+        //겹칠때?
+        /*
+        a, b와 가까운 임의의 지점 p에 대해 
+        s-p, a-p, b-p를 구하고
+        합치면 됨?
+        임의의 점 p는 s, a, b일 수도 있음
+        근데 p에서의 거리의 합이 가장 작은 경우는 특정 노드에 쏠릴 수 있음
+        */
+        int[] ap = dijikstra(a);
+        int[] bp = dijikstra(b);
+        int[] sp = dijikstra(s);
+        int answer = Integer.MAX_VALUE;
+        int idx = 0;
+        for(int i =1 ;i<=n; i++){
+            answer = Math.min(answer, sp[i] + ap[i] + bp[i]);
         }
-        
         return answer;
     }
     
-    int[] dijkstra(int start){
-        int[] dist = new int[n+1];
-        Arrays.fill(dist, max);
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        pq.offer(new Pair(start, 0));
-        dist[start] = 0;
+    public int[] dijikstra(int start){
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(start, 0));
+        int[] costs = new int[n+1];
+        Arrays.fill(costs, 30000000);
+        costs[start] = 0;
         
         while(!pq.isEmpty()){
-            Pair cur = pq.remove();
-            if(cur.cost > dist[cur.node]) continue;
-            for(Pair nxt : gr.get(cur.node)){
-                int nxtCost = cur.cost+nxt.cost;
-                if(dist[nxt.node] < nxtCost) continue;
-                pq.offer(new Pair(nxt.node, nxtCost));
-                dist[nxt.node] = nxtCost;
+            Edge cur = pq.poll();
+            
+            for(Edge next : gr.get(cur.to)){
+                int nextCost = cur.cost + next.cost;
+                if(costs[next.to] > nextCost){
+                    pq.offer(new Edge(next.to, nextCost));
+                    costs[next.to] = nextCost;
+                }
             }
         }
-        return dist;
+        
+        return costs;
     }
 }
-
-class Pair implements Comparable<Pair>{
-    int node, cost;
-    public Pair(int node, int cost){
-        this.node = node;
-        this.cost = cost;
-    }
-    
-    @Override
-    public int compareTo(Pair p){
-        return Integer.compare(this.cost, p.cost);
-    }
-}
-
-//시작 지점으로부터 각 지점까지의 거리만 구하는 다익스트라
-
