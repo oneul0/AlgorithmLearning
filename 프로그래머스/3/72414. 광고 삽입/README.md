eaa0b2ca4ff7a14834728655daaaf93e3d74c4e0
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-08-03 17:52:58 +0900

    [level 3] Title: 광고 삽입, Time: 175.79 ms, Memory: 342 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/README.md
+++ 프로그래머스/3/72414. 광고 삽입/README.md
@@ -1,10 +1,10 @@
 # [level 3] 광고 삽입 - 72414 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72414?language=java#fn1) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72414) 
 
 ### 성능 요약
 
-메모리: 312 MB, 시간: 344.53 ms
+메모리: 342 MB, 시간: 175.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 11일 21:55:29
+2026년 08월 03일 17:52:54
 
 ### 문제 설명
 
--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -1,52 +1,79 @@
+import java.util.*;
 class Solution {
     public String solution(String play_time, String adv_time, String[] logs) {
-        int playSecTime = calcToSecTime(play_time);
-        long[] timeStamp = new long[playSecTime + 1];
-
-        for (String log : logs) {
-            String[] times = log.split("-");
-            int startTime = calcToSecTime(times[0]);
-            int endTime = calcToSecTime(times[1]);
-            timeStamp[startTime]++;
-            timeStamp[endTime]--;
+        if(play_time.equals(adv_time)) return "00:00:00";
+        int playTotalTime = time2Sec(play_time);
+        int adTime = time2Sec(adv_time);
+        int[] viewerCnt = new int[playTotalTime+1];
+        
+        //누적합으로 해당 부분의 누적 시청자 수 구하기
+        for(String log : logs){
+            int[] chunk = log2Sec(log);
+            viewerCnt[chunk[0]]++;
+            viewerCnt[chunk[1]]--;
         }
-
-        for (int i = 1; i <= playSecTime; i++) {
-            timeStamp[i] += timeStamp[i - 1];
+        
+        // 시청자 수 변화량 → 초별 동시 시청자 수
+        for(int sec = 1; sec<playTotalTime; sec++){
+            viewerCnt[sec] += viewerCnt[sec-1];
         }
-
-        for (int i = 1; i <= playSecTime; i++) {
-            timeStamp[i] += timeStamp[i - 1];
+        
+        //슬라이딩 윈도우
+        long window = 0; //전체 누적 청자 수
+        for(int i = 0; i<adTime; i++){
+            window += viewerCnt[i];
         }
-
-        int adSecTime = calcToSecTime(adv_time);
-        long result = timeStamp[adSecTime - 1];
-        int idx = 0;
-
-        for (int i = adSecTime; i <= playSecTime; i++) {
-            long totalView = timeStamp[i] - timeStamp[i - adSecTime];
-            if (totalView > result) {
-                result = totalView;
-                idx = i - adSecTime+1;
+        
+        long maxWatchTime = window;
+        int bestStartTime = 0;
+        
+        for(int start = 1; start + adTime <= playTotalTime; start++){
+            int removedSec = start-1;
+            int addedSec = start + adTime -1;
+            
+            window -= viewerCnt[removedSec];
+            window += viewerCnt[addedSec];
+            
+            if(window > maxWatchTime){
+                maxWatchTime = window;
+                bestStartTime = start;
             }
         }
-
-        return secTimeToFormattedTime(idx);
+        
+        return sec2Log(bestStartTime);
     }
-
-    public int calcToSecTime(String time) {
-        String[] hhMmSs = time.split(":");
-        int hh = Integer.parseInt(hhMmSs[0]) * 3600;
-        int mm = Integer.parseInt(hhMmSs[1]) * 60;
-        int ss = Integer.parseInt(hhMmSs[2]);
-        return hh + mm + ss;
+    
+    
+    //utils
+    public String sec2Log(int startTime){
+        int start = startTime;
+        int startHH = start / 3600;
+        
+        start %= 3600;
+        int startMM = start / 60;
+        int startSS = start % 60;
+        
+        return makeTwo(startHH) + ":" + makeTwo(startMM) + ":" + makeTwo(startSS);
     }
-
-    public String secTimeToFormattedTime(long time) {
-        long hh = time / 3600;
-        time %= 3600;
-        long mm = time / 60;
-        long ss = time % 60;
-        return String.format("%02d:%02d:%02d", hh, mm, ss);
+    
+    //"01:20:15" 형태
+    public int time2Sec(String time){
+        String[] hhmmss = time.split(":");
+        int sec = Integer.parseInt(hhmmss[0])*3600
+            + Integer.parseInt(hhmmss[1])*60
+            + Integer.parseInt(hhmmss[2]);
+        
+        return sec;
+    }
+    public int[] log2Sec(String log){
+        String[] startTimeEndTime = log.split("-");
+        int startTime = time2Sec(startTimeEndTime[0]);
+        int endTime = time2Sec(startTimeEndTime[1]);
+        return new int[]{startTime, endTime};
+    }
+    
+    public String makeTwo(int time){
+        String str = String.valueOf(time);
+        return str.length() < 2 ? "0"+str : str;
     }
 }
