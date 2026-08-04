5e4b8a76ae7a6cb498b97f0f19b6cb461dfc22f2
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-08-04 10:17:28 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.08 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,15 +1,17 @@
 class Solution {
-    final int MOD = 1234567;
-    
     public long solution(int n) {
-        if(n == 1) return 1;
-        if(n == 2) return 2;
+        if(n==1) return 1;
+        if(n==2) return 2;
+        final int MOD = 1234567;
         long[] dp = new long[n+1];
+        
+        //dist = i일때
         dp[1] = 1;
         dp[2] = 2;
         for(int i = 3; i<=n; i++){
-            dp[i] = (dp[i-1]+dp[i-2])%MOD;
+            dp[i] = (dp[i-1] + dp[i-2])%MOD;
         }
+        
         return dp[n];
     }
 }
