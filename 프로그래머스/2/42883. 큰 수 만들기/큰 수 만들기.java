a738d28c3576fc2b0a10126811fafb46ae91db69
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-08-06 21:30:07 +0900

    [level 2] Title: 큰 수 만들기, Time: 16.13 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,20 +1,25 @@
+import java.util.*;
 class Solution {
     public String solution(String number, int k) {
-        StringBuilder answer = new StringBuilder("");
-        int len = number.length() - k;
-        int start = 0;
+        StringBuilder sb = new StringBuilder();
+        sb.append(number.charAt(0));
         
-        while(start < number.length() && answer.length() != len) {
-            int leftNum = k + answer.length() + 1;
-            int max = 0;
-            for (int j = start; j < leftNum; j++) {
-                if (max < number.charAt(j) - '0') {
-                    max = number.charAt(j) - '0';
-                    start = j + 1;
-                }
+        for(int i =1; i<number.length(); i++){
+            //이전에 온 수가 지금 만난 수보다 더 작은 경우
+            while(k > 0
+                  && sb.length()>0
+                  && sb.charAt(sb.length()-1) < number.charAt(i)){
+                sb.deleteCharAt(sb.length()-1);
+                k--;
             }
-            answer.append(Integer.toString(max));
+            sb.append(number.charAt(i));
         }
-        return answer.toString();
+        if(k>0){
+            sb.delete(sb.length()-k, sb.length());
+        }
+        return sb.toString();
     }
 }
+//단조감소스택으로 다음과 같이 구현
+//이전의 값이 이후에 오는 값보다 작으면 pop 
+//
