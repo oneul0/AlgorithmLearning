b6b7edf717e4f99652abd7fb5f37aa19e434223f
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-08-12 17:52:01 +0900

    [level 3] Title: 정수 삼각형, Time: 6.44 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,25 +1,28 @@
-import java.util.*;
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = 0;
-        int[][] dp = new int[triangle.length+1][triangle[triangle.length-1].length+1];
+        int[][] dp = new int[triangle.length][triangle[triangle.length-1].length];
         dp[0][0] = triangle[0][0];
-        for(int i = 1 ; i<triangle.length; i++){
+        for(int i = 1; i<triangle.length; i++){
             for(int j = 0; j<triangle[i].length; j++){
-                if(j == 0) dp[i][j] = dp[i-1][j]+triangle[i][j];
-                else if(j == triangle[i].length-1) dp[i][j] = dp[i-1][j-1]+triangle[i][j];
-                else dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j])+triangle[i][j];
-                answer = Math.max(answer, dp[i][j]);
+                //왼쪽 끝은 바로 위(i-1, j)만 가능
+                if(j == 0){
+                    dp[i][j] = triangle[i][j]+ dp[i-1][j];
+                }
+                //오른쪽 끝은(i-1, j-1)만 가능
+                else if(j == triangle[i].length-1){
+                    dp[i][j] = triangle[i][j]+ dp[i-1][j-1];
+                }
+                //나머지는 위(i-1,j), 왼쪽(i-1,j-1) 가능
+                else{
+                    dp[i][j] = triangle[i][j]+ Math.max(dp[i-1][j], dp[i-1][j-1]);
+                }
             }
         }
         
+        int answer = 0;
+        for(int val : dp[dp.length-1]){
+            answer = Math.max(answer, val);
+        }
         return answer;
     }
 }
-
-/*
-이동할 수 있는 경우
-1. 왼쪽 i -> i
-2. 오른쪽 i -> i+1
-
-*/
