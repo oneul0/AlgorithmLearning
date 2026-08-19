2a815dfbdddc9e17a9de0aee7e1992a092edc977
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-08-19 19:48:15 +0900

    [D4] Title: 치즈 도둑, Time: 328 ms, Memory: 95,592 KB -BaekjoonHub

--- SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -1,74 +1,68 @@
 import java.util.*;
 import java.io.*;
+
 class Solution
 {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int[] dx = {0,0,-1,1};
-    static int[] dy = {1,-1,0,0};
-    public static void main(String args[]) throws Exception {
-
-        int T = Integer.parseInt(br.readLine());
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-            int N = Integer.parseInt(br.readLine());
-            int[][] cheese = new int[N][N];
-            for(int i = 0; i < N; i++){
-                String[] in = br.readLine().split(" ");
-                for(int j = 0; j < in.length; j++){
-                    cheese[i][j] = Integer.parseInt(in[j]);
+	public static void main(String args[]) throws Exception
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int N = Integer.parseInt(br.readLine());
+            int[][] arr = new int[N][N];
+            StringTokenizer st;
+            for(int i = 0; i<N; i++){
+                st = new StringTokenizer(br.readLine());
+                for(int j = 0; j<N; j++){
+                    arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-
-            bw.write("#"+test_case+" "+simulation(cheese, N)+"\n");
-            bw.flush();
-
-        }
-        bw.close();
+            
+            
+            //days gone
+            int maxGroup =  0;
+            for(int day = 1; day<=100; day++){
+	            maxGroup = Math.max(maxGroup, groupCount(arr, day, N));
+            }
+            bw.write("#"+test_case+" "+maxGroup+"\n");
+		}
+        bw.flush();
         br.close();
-    }
-
-    static int simulation(int[][] cheese, final int N){
-        int ans = 0;
-        for(int i = 0; i <= 100; i++){
-            ans = Math.max(ans, chuncks(cheese, i, N));
-        }
-        return ans;
-    }
-
-    static int chuncks(int[][] cheese, int day, final int N){
-        boolean[][] chk = new boolean[cheese.length][cheese[0].length];
-        int cnt = 0;
-        for(int i = 0; i<cheese.length; i++){
-            for(int j = 0; j<cheese[0].length; j++){
-                if(cheese[i][j] > day && !chk[i][j]){
-                    bfs(i,j,cheese,chk, day, N);
-                    cnt++;
+        bw.close();
+	}
+    
+    static int[] dx = {-1,1,0,0}, dy = {0,0,-1,1};
+    public static int groupCount(int[][] arr, int day, int N){
+        int count = 0;
+        boolean[][] visited = new boolean[N][N];
+        for(int i = 0; i<N; i++){
+            for(int j = 0; j<N; j++){
+                if(!visited[i][j] && day<= arr[i][j]){
+                    count++;
+                    bfs(arr, day, N, visited, i, j);
                 }
             }
         }
-        return cnt;
+        return count;
     }
-
-    static void bfs(int sx, int sy, int[][] cheese, boolean[][] chk, int day, final int N){
-        Deque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{sx,sy});
-
-        chk[sx][sy] = true;
-
+    public static void bfs(int[][] arr, int day, int N, boolean[][] visited, int sx, int sy){
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{sx, sy});
+		visited[sx][sy] = true;
+        
         while(!q.isEmpty()){
-            int[] cur = q.remove();
-
+         	int[] cur = q.poll();
             for(int i = 0; i<4; i++){
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
-                if(nx<0||nx>=N|| ny<0||ny>=N || chk[nx][ny]) continue;
-                if(cheese[nx][ny] <= day) {
-                    chk[nx][ny] = true;
-                    continue;
-                }
-                chk[nx][ny] = true;
-                q.offer(new int[]{nx,ny});
+                if(nx<0 || ny<0 || nx>=N || ny>=N || visited[nx][ny]) continue;
+                if(arr[nx][ny] < day) continue;
+                q.offer(new int[]{nx, ny});
+                visited[nx][ny] = true;
             }
         }
     }
