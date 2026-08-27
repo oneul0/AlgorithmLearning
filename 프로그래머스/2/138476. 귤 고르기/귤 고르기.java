86291cb25095488d58d296dbe87be4c5bd5f5c9b
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-08-28 00:35:34 +0900

    [level 2] Title: 귤 고르기, Time: 27.28 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,36 +1,18 @@
 import java.util.*;
 class Solution {
-    class Pair implements Comparable<Pair> {
-        int size;
-        int amount;
-        Pair(int size, int amount){
-            this.size = size;
-            this.amount = amount;
-        }
-        @Override
-        public int compareTo(Pair o){
-            return o.amount - this.amount;
-        }
-    }
     public int solution(int k, int[] tangerine) {
         Map<Integer, Integer> map = new HashMap<>();
-
-        for (int t : tangerine) {
-            map.put(t, map.getOrDefault(t, 0) + 1);
+        for(int i = 0; i<tangerine.length; i++){
+            map.compute(tangerine[i], (key, value) -> value == null ? 1 : ++value);
         }
-
         List<Integer> counts = new ArrayList<>(map.values());
         counts.sort(Collections.reverseOrder());
-
-        int answer = 0;
-        int sum = 0;
-
-        for (int c : counts) {
-            sum += c;
+        int answer = 0, count = 0;
+        for (int c : counts){
+            count += c;
             answer++;
-            if (sum >= k) break;
+            if(k<=count) return answer;
         }
-
-        return answer;
+        return 0;
     }
 }
