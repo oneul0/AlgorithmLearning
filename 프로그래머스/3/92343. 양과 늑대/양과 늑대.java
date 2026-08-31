3b6dc3b8bd14e61ebc6105901d4c38e40f950323
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-08-31 19:33:56 +0900

    [level 3] Title: 양과 늑대, Time: 4.98 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,30 +1,45 @@
+import java.util.*;
 class Solution {
+    int n, answer = 0;
     int[] info;
-    int[][] edges;
-    int max = 0;
+    List<List<Integer>> gr = new ArrayList<>();
     public int solution(int[] info, int[][] edges) {
+        this.n = info.length;
         this.info = info;
-        this.edges = edges;
         
-        dfs(0, 0, 0, new boolean[info.length]);
-        
-        return max;
-    }
-    //DFS(현재 노드 번호, 양의 수, 늑대의 수, 다음으로 방문할 수 있는 노드의 집합)
-    void dfs(int cur, int sheep, int wolf, boolean[] chk) {
-        chk[cur] = true;
-        int cur_sheep = (sheep + (info[cur]^1));
-        int cur_wolf = (wolf + info[cur]);
-        
-        max = Math.max(max, cur_sheep);
-        //양보다 늑대가 많아지면 탐색할 수 없으므로 리턴
-        if(cur_sheep <= cur_wolf) return;
-        
+        for(int i = 0; i<n; i++){
+            gr.add(new ArrayList<>());
+        }
         for(int[] edge : edges){
-            if(chk[edge[0]] && !chk[edge[1]]){
-                //각 노드에서의 방문 체크를 독립적으로 관리
-                dfs(edge[1], cur_sheep, cur_wolf, chk.clone());
-            }
+            gr.get(edge[0]).add(edge[1]);
+        }
+        Set<Integer> canGo = new HashSet<>();
+        canGo.add(0);
+        dfs(0, 0, canGo);
+        return answer;
+    }
+    public void dfs(int sheep, int wolf, Set<Integer> canGo){
+        answer = Math.max(answer, sheep);
+        
+        for(int next : canGo){
+            int nextS = sheep;
+            int nextW = wolf;
+            
+            if(info[next] == 1) nextW++;
+            else nextS++;
+            
+            if(nextW>=nextS) continue;
+            
+            Set<Integer> newCanGo = new HashSet<>(canGo);
+            newCanGo.remove(next);
+            newCanGo.addAll(gr.get(next));
+            
+            dfs(nextS, nextW, newCanGo);
         }
     }
 }
+//한번 갔던 노드는 다시 갈 수 있음이 보장됨
+//set을 이용해서 평균 O(1)로 노드로 이동할 수 있음
+//2차원 리스트를 이용해서 트리의 노드를 하나씩 순회할 경우 양방향으로 관리해야 문제의 제약을 지킬 수 있지만
+//이동할 수 있는 노드를 관리하는 배열을 사용한다면 각각의 노드에서 이동할 수 있는 모든 노드를 수집하며 경우의 수를 쌓아갈 수 있다
+//또한, 최대로 모을 수 있는 양의 수를 각 상태에서 업데이트한다면 기저조건에 넣지 않아도 최대값을 구할 수 있다.
