df140804ef226f58cba8851d01876789b343bde6
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-09-01 09:28:08 +0900

    [Passed] 실수 입력받아 계산 (104ms, 9MB)

---
+++ trail1/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double a = Double.parseDouble(br.readLine());
+        double b = Double.parseDouble(br.readLine());
+        System.out.printf("%.2f", a+b);
+    }
+}
