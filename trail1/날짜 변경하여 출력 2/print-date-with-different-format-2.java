8c08629f6fcf11cb8467803099326ed5e2081136
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-09-01 09:41:09 +0900

    [Passed] 날짜 변경하여 출력 2 (93ms, 9MB)

---
+++ trail1/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split("-");
+        System.out.print(str[2]+"."+str[0]+"."+str[1]);
+    }
+}
