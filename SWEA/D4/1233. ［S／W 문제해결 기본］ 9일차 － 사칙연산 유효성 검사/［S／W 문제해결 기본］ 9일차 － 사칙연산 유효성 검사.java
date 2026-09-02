57171e538020a94663c16b66c89371f2bdf798e7
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-09-02 14:20:41 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 285 ms, Memory: 33,152 KB -BaekjoonHub

---
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+		for(int test_case = 1; test_case <= 10; test_case++)
+		{
+            bw.write("#"+test_case+" ");
+            
+            StringTokenizer st;
+			int N = Integer.parseInt(br.readLine());
+            boolean valid = true;
+            for(int i = 0; i<N; i++){
+                st = new StringTokenizer(br.readLine());
+                int cur = Integer.parseInt(st.nextToken());
+                String val = st.nextToken();
+                
+                boolean isNumber = val.chars().allMatch(Character::isDigit);
+                boolean hasChild = st.hasMoreTokens();
+                
+                if(isNumber && hasChild || !isNumber && !hasChild) {
+                    valid = false;
+                }
+            }
+            bw.write(valid ? "1" : "0");
+            bw.newLine();
+            bw.flush();
+		}
+        
+        br.close();
+        bw.close();
+	}
+}
