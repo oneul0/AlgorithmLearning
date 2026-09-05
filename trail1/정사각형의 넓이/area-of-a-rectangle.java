dd1c491b23792006be7a5ffe488d1e546cb32d84
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-09-05 15:29:09 +0900

    [Passed] 정사각형의 넓이 (88ms, 9MB)

---
+++ trail1/정사각형의 넓이/area-of-a-rectangle.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.print(n*n +"\n"+ (n<5 ? "tiny" : ""));
+    }
+}
