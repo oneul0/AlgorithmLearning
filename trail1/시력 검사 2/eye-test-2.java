eca1696af60db07ec812a4a7ebbbd03fd79bf18d
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-09-06 16:02:35 +0900

    [Passed] 시력 검사 2 (94ms, 9MB)

---
+++ trail1/시력 검사 2/eye-test-2.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double a = Double.parseDouble(br.readLine());
+        if(a>=1.0) System.out.print("High");
+        else if(a>=0.5) System.out.print("Middle");
+        else System.out.print("Low");
+    }
+}
