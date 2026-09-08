1b629f808c1f13e6cde952a161a6923bc0587df6
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-09-09 08:09:23 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.56 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,50 +1,42 @@
 import java.util.*;
 class Solution {
-    ArrayList<ArrayList<Integer>> tree;
-    int answer = 30000;
+    List<List<Integer>> gr = new ArrayList<>();
     public int solution(int n, int[][] wires) {
-        tree =  new ArrayList<>(n+1);
-        for(int i = 0; i<n+1; i++){
-            tree.add(new ArrayList<>());
+        for(int i = 0; i<=n; i++){
+            gr.add(new ArrayList<>());
         }
-        // 1. 무방향 그래프로 바꾸기
-        for(int[] wire : wires){
-            tree.get(wire[0]).add(wire[1]);
-            tree.get(wire[1]).add(wire[0]);
+        for(int[] w : wires){
+            gr.get(w[0]).add(w[1]);
+            gr.get(w[1]).add(w[0]);
         }
-        
-        // 2. 탐색을 시작할 정점과 끊어낼 정점 정하기(어느 간선을 끊을지)
-        for(int[] wire : wires){
+        int answer = 987654321;
+        //하나 정해서 끊기
+        //정점 하나 체크하면 간선도 다 끊기는 효과니까
+        for(int i =1; i<=n; i++){
             boolean[] visited = new boolean[n+1];
-            //주어진 간선 정보는 이어질 수 있는 간선의 모든 경우의 수이므로
-            //wires를 순회하는 것 만으로도 모든 경우의 수를 구할 수 있음
-            //이어진 간선을 끊기 위해 v2를 구해 방문처리
-            int v1 = wire[0], v2=wire[1];
-            // 2.1 간선 끊기
-            visited[v2] = true;
-            
-            //dfs가 더해준 count값 받기
-            // 2.2 트리 하나의 정점 수 구하기
-            int count = dfs(v1,visited);
-            //짝수로 만들어서 나뉜 전력망 중 어느것으로 빼도 같은 값이 나오도록
-            // 2.3 차이가 최소가 될 때의 개수 구하기
-            answer = Math.min(answer, Math.abs(n-2*count));
+            visited[i] = true;
+            int o = bfs(visited);
+            answer = Math.min(answer, Math.abs(o+o-n));
         }
         
         return answer;
     }
-    
-    int dfs(int cur, boolean[] visited){
-        visited[cur] = true;
-        
-        int count = 1;
-        
-        for(int nxt : tree.get(cur)){
-            if(!visited[nxt]){
-                count += dfs(nxt, visited);
+    public int bfs(boolean[] visited){
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(1);
+        visited[1] = true;
+        int result = 1;
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            
+            for(int next : gr.get(cur)){
+                if(!visited[next]){
+                    q.offer(next);
+                    visited[next] = true;
+                    result++;
+                }
             }
         }
-        
-        return count;
+        return result;
     }
 }
