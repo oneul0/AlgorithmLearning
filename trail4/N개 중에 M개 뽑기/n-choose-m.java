78278ac90aba0612ac0a165a18e9910f7dd0086e
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-09-08 15:41:51 +0900

    [Passed] N개 중에 M개 뽑기 (137ms, 10MB)

--- trail4/N개 중에 M개 뽑기/n-choose-m.java
+++ trail4/N개 중에 M개 뽑기/n-choose-m.java
@@ -2,18 +2,19 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int n, m;
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        comb(1, n, m, new int[m], 0, 0);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        comb(1, new int[m], 0, 0);
         
 
         bw.flush();
         bw.close();
     }
-    public static void comb(int start, int n, int m, int[] arr, int idx, int mask) throws IOException {
+    public static void comb(int start, int[] arr, int idx, int mask) throws IOException {
         if(idx == m){
             for(int v : arr){
                 bw.write(v+" ");
@@ -26,7 +27,7 @@ public class Main {
             if((mask & (1<<i)) != 0) continue;
             mask |= (1<<i);
             arr[idx] = i;
-            comb(i+1, n, m, arr, idx+1, mask);
+            comb(i+1, arr, idx+1, mask);
             mask &= ~(1<<i);
         }
     }
