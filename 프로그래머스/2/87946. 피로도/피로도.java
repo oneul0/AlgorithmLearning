903187e182f64e75ef1f562716b4d2abbbf4d2d1
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-09-10 14:34:04 +0900

    [level 2] Title: 피로도, Time: 2.32 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,6 +1,21 @@
 class Solution {
+    int n;
+    int max = 0;
+    int[][] dungeons;
     public int solution(int k, int[][] dungeons) {
-        int answer = -1;
-        return answer;
+        this.n = dungeons.length;
+        this.dungeons = dungeons;
+        comb(k, 0);
+        return max;
+    }
+    
+    public void comb(int hp, int mask){
+        max = Math.max(max, Integer.bitCount(mask));
+        
+        for(int i = 0; i<n; i++){
+            if(hp >= dungeons[i][0] && ((mask & (1<<i)) == 0)){
+                comb(hp-dungeons[i][1], mask | (1<<i));
+            }
+        }
     }
 }
