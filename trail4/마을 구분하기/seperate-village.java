5d03c02823bfab32c542c00f7f95e434370a43f7
KimKiHoon <54806810+oneul0@users.noreply.github.com>
2026-09-22 10:58:45 +0900

    [Passed] 마을 구분하기 (91ms, 9MB)

--- trail4/마을 구분하기/seperate-village.java
+++ trail4/마을 구분하기/seperate-village.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
@@ -5,15 +6,19 @@ public class Main {
     static int[][] grid;
     static boolean[][] visited;
     static int[] dx = {-1,1,0,0}, dy = {0,0,-1,1};
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
         grid = new int[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                grid[i][j] = sc.nextInt();
-                
-        
+        for (int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++){
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         visited = new boolean[n][n];
         for(int i =0 ; i<n; i++){
@@ -25,13 +30,14 @@ public class Main {
                 }
             }
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append(pq.size()).append("\n");
+        
+        bw.write(pq.size()+"\n");
         while(!pq.isEmpty()){
-            sb.append(pq.poll()).append("\n");
+            bw.write(pq.poll()+"\n");
         }
-        System.out.print(sb.toString());
-
+        bw.flush();
+        br.close();
+        bw.close();
     }
 
     public static void dfs(int x, int y){
